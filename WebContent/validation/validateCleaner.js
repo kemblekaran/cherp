6eeed1a2f6fd1fdf5f59db56867d6d832900d025
kemblekaran <kemblekaran@gmail.com>
2017-08-10 23:23:42 +0530

    Added validation rules for customer like pan,adhaar and driving lisence pattern validation

--- WebContent/validation/validateCleaner.js
+++ WebContent/validation/validateCleaner.js
@@ -1,6 +1,22 @@
-$(function(){
+$(function() {
 
-	//validates the cleaner form
+	// Pattern for pan card number validation handler
+	$.validator.addMethod("pan", function(value, element) {
+		return this.optional(element) || /^[A-Z]{5}\d{4}[A-Z]{1}$/.test(value);
+	}, "Invalid Pan Number");
+
+	// Pattern for driving lisense validation handler
+	$.validator.addMethod("lisence", function(value, element) {
+		return this.optional(element) || /^[A-Z]{2}-\d{14}$/.test(value);
+	}, "Invalid Lisence Number");
+
+	//Patter for Adhaar formatter
+	$.validator.addMethod("adhaar", function(value, element) {
+		return this.optional(element) || /^\d{4}-\d{4}-\d{4}$/.test(value);
+	}, "Invalid Adhaar Format");
+	
+	
+	// validates the cleaner form
 	$('#InsertForm').validate({
 		errorClass : "red-error",
 		errorElement : "em",
@@ -11,16 +27,49 @@ $(function(){
 			per_add : "required",
 			state : "required",
 			city : "required",
-			mobile : "required",
-			phone : "required",
-			dr_lisence : "required",
-			pan_no : "required",
-			adhaar_no : "required",
+			mobile : {
+				required : true,
+				minlegth : 10,
+				maxlength : 10
+			},
+			phone : {
+				required : true,
+				minlegth : 10,
+				maxlength : 10
+			},
+			dr_lisence : {
+				required : true,
+				lisence : true,
+				minlength: 17,
+				maxlength: 17
+			},
+			pan_no : {
+				required : true,
+				pan : true
+			},
+			adhaar_no : {
+				required : true,
+				adhaar: true,
+				minlength : 14,
+				maxlength : 14
+			},
 			photo : "required"
 		},
 		messages : {
 			photo : {
 				required : 'Upload your Photo'
+			},
+			adhaar_no:{
+				required: 'Enter UID Number',
+				adhaar: 'Add a hyphen between Adhaar Number',
+				minlength: 'Adhaar is not Valid',
+				maxlength: 'Enter Valid Adhaar Number'
+			},
+			dr_lisence:{
+				required: 'Enter Your Lisence Number',
+				lisence: 'Liscence is invalid or add a hyphen between letters and numbers',
+				minlength: 'Liscence must be 17 characters long',
+				maxlength: 'Liscence Number must not contains more than 17 characters'
 			}
 		}
 	});
