9a06bf70b331e0044f64c5495a34779e25f1c1d2
kemblekaran <kemblekaran@gmail.com>
2017-08-23 15:21:28 +0530

    Created purchase.js and adds row to the table

---
+++ WebContent/js/dataentry/purchase.js
@@ -0,0 +1,96 @@
+$(function() {
+
+	var productJsonArray = [];
+	var product = $('#productSelect');
+	var kg = $('#kg');
+	var pieces = $('#pieces');
+	var rate = $('#rate');
+	var amt = $('#amount');
+	var avgWeight = $('#avgWeight');
+
+	var json;
+
+	// calculate amount and avg weight
+	rate.on('input', function() {
+
+		var newAmt = parseInt(rate.val()) * parseInt(pieces.val());
+		var newAvgWeight = parseInt(kg.val()) / parseInt(pieces.val());
+		if (newAmt !== null || newAvgWeight !== null) {
+			amt.val(newAmt);
+			avgWeight.val(newAvgWeight);
+		}
+	});
+
+	// set purchase id value
+	$.getJSON('/server/jsonfiles/purchaseView.json', function(result) {
+		var length = result.data.length;
+		var purchaseId = result.data[length - 1].purchaseId;
+
+		$('#purchaseid').val(purchaseId + 1);
+	});
+
+	// on double click in purchaseView send data to SalesServlet
+	$('#purchaseViewTable tbody').on('dblclick', 'tr', function() {
+		// By adding this to the row() it will select the current row
+		var purchaseId = dataTable.row(this).data().purchaseId;
+		console.log(purchaseId);
+		$.ajax({
+			url : 'SalesServlet',
+			async : false,
+			data : {
+				purchaseId : purchaseId,
+				purchaseView : 'true'
+			},
+			type : 'post',
+			success : function(data) {
+				console.log('Purchase successfully retrieved in sales!');
+			},
+			error : function() {
+				console.log('error in purchase double click ajax!');
+			}
+		});
+	});
+
+	$('#purchaseViewTable tbody').on('dblclick', function() {
+		window.location = 'Sales.html';
+	});
+
+	$('#productTable tbody tr td').keydown(
+			function(e) {
+
+				var productRowData = [];
+
+				var productRow = {
+					"product" : product.val(),
+					"pieces" : pieces.val(),
+					"kg" : kg.val(),
+					"rate" : rate.val(),
+					"amt" : amt.val(),
+					"avgWeight" : avgWeight.val()
+				}
+
+				if (e.keyCode === 13) {
+					console.log(productRow.product);
+					productRowData.push(productRow);
+					
+					productTable.row.add(
+							[ product.val(), pieces.val(), kg.val(),
+									rate.val(), amt.val(), avgWeight.val() ])
+							.draw();
+					json = JSON.stringify(productRow);
+					
+					console.log('Json' + productJsonArray);
+					console.log(productRowData);
+				}
+			});
+
+	
+	// to prevent form submission on enter key press
+	$(window).keydown(function(event) {
+		if (event.keyCode == 13) {
+			event.preventDefault();
+			return false;
+		}
+	});
+
+});
